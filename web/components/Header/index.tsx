50a80461246e7b5368f05bd7a4b7e38d0d6ff830
rob shaw <rob@statik.dev>
2022-02-25 07:17:20 -0500

    refactor: move monkedao x smb logo to header

--- web/components/Header/index.tsx
+++ web/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 import styles from './index.module.css';
@@ -5,6 +7,14 @@ import styles from './index.module.css';
 export default function Header() {
   return (
     <div className={styles.container}>
+      <div className="d-flex w100 justify-content-center align-items-center pb-2">
+        <Image
+          src="/daosmb.png"
+          alt="smb and monkeDAO logo"
+          width="400px"
+          height="90.5px"
+        />
+      </div>
       <div className={styles.walletButtons}>
         <WalletMultiButton />
       </div>
--- web/components/Layout/index.tsx
+++ web/components/Layout/index.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 import Header from '../Header';
 import Footer from '../Footer';
 
@@ -7,17 +5,6 @@ export default function Layout({ children }: { children: any }) {
   return (
     <>
       <Header />
-      <div className="d-flex w100 justify-content-center align-items-center pb-2">
-        <Image
-          src="/daosmb.png"
-          alt="smb and monkeDAO logo"
-          width="400px"
-          height="90.5px"
-        />
-      </div>
-      <div className="d-flex w100 justify-content-center align-items-center pb-2 mt-1">
-        <h1 className="display-4 fw-normal">DAO Vote</h1>
-      </div>
       <main>{children}</main>
       <Footer />
     </>
